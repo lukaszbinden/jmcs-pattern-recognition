5096bbee154b13d640b091e09e2ac54c4ff6bd5f
lunkford <fabiodegiacomi@gmail.com>
2018-04-09 13:44:12 +0200

    got started on the svm thing

--- Exercise_2a/svm.py
+++ Exercise_2a/svm.py
@@ -8,24 +8,33 @@ test = np.genfromtxt('../data/MNIST/test.csv', delimiter=',')
 
 print ('training model...')
 
-y = train[:, 0]
-X = train[:, 1:]
+# complexity is quadratic (?), so much more than 10'000 samples might not be possible
 
-y_test = test[:10, 0]
-X_test = test[:10, 1:]
+y = train[:10000, 0]
+X = train[:10000, 1:]
 
-svm = SVC(verbose=True, kernel='linear')
+y_test = test[:, 0]
+X_test = test[:, 1:]
+
+svm = SVC(verbose=True, kernel='rbf')
 svm.fit(X, y)
 
 print ('predicting...')
 
 pred = svm.predict(X_test)
 
-#print (pred)
-#print (y_test)
+print (pred)
+print (y_test)
 
-acc = (y_test == pred).sum() / len(pred)
+acc = ((y_test == pred).sum() + 0.0) / len(pred)
 
 print ('done.')
 
 print ('Accuracy: %3f ' % acc)
+
+
+# Results:
+#
+# kernel=linear, C=1.0, gamma=1/#features -> Acc=0.913806
+# kernel=rbf   , C=1.0, gamma=1/#features -> Acc= <still running>
+#
