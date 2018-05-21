c07b9328f025705d90c8890324610e1b4af27ff8
Lukas Zbinden <lukas.zbinden@unifr.ch>
2018-05-21 10:22:56 +0200

    implement edge assignment cost function

--- Exercise_4/src/ch/unifr/jmcs/patrec/ex04/algo/ICostFunction.java
+++ Exercise_4/src/ch/unifr/jmcs/patrec/ex04/algo/ICostFunction.java
@@ -1,5 +1,7 @@
 package ch.unifr.jmcs.patrec.ex04.algo;
 
+import net.sourceforge.gxl.GXLEdge;
+
 public interface ICostFunction {
 
 	double nodeInsertion(Node n1);
@@ -7,6 +9,8 @@ public interface ICostFunction {
 	double nodeDeletion(Node n1);
 	
 	double nodeSubstitution(Node n1, Node n2);
+	
+	double edgeSubstitution(GXLEdge e1, GXLEdge e2);
 	
 	double edgeInsertion();
 	
